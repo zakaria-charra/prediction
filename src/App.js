9163748286e070795dbc8e4e5032685d1ae75aae
zakatia-charra <you@examplegit config --global user.name Your>
2021-11-04 02:12:01 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Dashb from './Components/Dashb';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashb/>
+    </div>
+  );
+}
+
+export default App;
